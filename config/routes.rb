d2b2de57d321e96a7042f9229bc8a85be5ad7ff5
Carson Jardine <carsonjardine@gmail.com>
2020-10-10 16:07:16 -0600

    Update code to include pets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #shelters
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
@@ -10,4 +12,6 @@ Rails.application.routes.draw do
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
 
+  #pets
+  get '/pets', to: 'pets#index'
 end
