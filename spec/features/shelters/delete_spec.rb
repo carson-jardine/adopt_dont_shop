c476a0c948eb027c7e8a3a8fb1bca88df35609aa
Carson Jardine <carsonjardine@gmail.com>
2020-10-10 14:49:02 -0600

    Add code for delete test

--- spec/features/shelters/delete_spec.rb
+++ spec/features/shelters/delete_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
+describe 'As a visitor' do
+  describe 'When I visit /shelters/:id' do
+    it "The visitor can delete a shelter" do
+      @shelter_1 = Shelter.create(name:     "Denver Animal Shelter",
+                                  address:  "1241 W Bayaud Avenue",
+                                  city:     "Denver",
+                                  state:    "CO",
+                                  zip:      "80223")
 
-# User Story 6, Shelter Delete
-#
-# As a visitor
-# When I visit a shelter show page
-# Then I see a link to delete the shelter
-# When I click the link "Delete Shelter"
-# Then a 'DELETE' request is sent to '/shelters/:id',
-# the shelter is deleted,
-# and I am redirected to the shelter index page where I no longer see this shelter
+      visit "/shelters/#{@shelter_1.id}"
+      click_on "Delete Shelter"
+
+      expect(page).to_not have_content("Denver Animal Shelter")
+    end
+  end
+end
