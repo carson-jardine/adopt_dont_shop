0cde9c40ed17b999daee0abaa242629f46a6fc62
Carson Jardine <carsonjardine@gmail.com>
2020-10-10 14:16:36 -0600

    Fix name param and spacing

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -12,12 +12,12 @@ class SheltersController < ApplicationController
 
   def create
     shelter = Shelter.new({
-              name: params[:shelter][:name],
-              address: params[:shelter][:address],
-              city: params[:shelter][:city],
-              state: params[:shelter][:state],
-              zip: params[:shelter][:zip]
-              })
+                name: params[:shelter][:name],
+                address: params[:shelter][:address],
+                city: params[:shelter][:city],
+                state: params[:shelter][:state],
+                zip: params[:shelter][:zip]
+                })
     shelter.save
 
     redirect_to '/shelters'
@@ -27,5 +27,18 @@ class SheltersController < ApplicationController
     @shelter = Shelter.find(params[:id])
   end
 
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update({
+              name: params[:shelter][:name],
+              address: params[:shelter][:address],
+              city: params[:shelter][:city],
+              state: params[:shelter][:state],
+              zip: params[:shelter][:zip]
+              })
 
+    shelter.save
+
+    redirect_to "/shelters/#{shelter.id}"
+  end
 end
