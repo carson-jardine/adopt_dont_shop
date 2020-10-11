dcfbb7bee251ce58ac2adea96418534a860b6eef
Carson Jardine <carsonjardine@gmail.com>
2020-10-11 12:19:14 -0600

    Update shelter pets controller with new and create methods

--- app/controllers/shelter_pets_controller.rb
+++ app/controllers/shelter_pets_controller.rb
@@ -4,4 +4,23 @@ class ShelterPetsController < ApplicationController
     @pets = Shelter.find(params[:id]).pets
   end
 
+  def new
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def create
+    pet = Pet.new({
+            name: params[:pet][:name],
+            image: params[:pet][:image],
+            description: params[:pet][:description],
+            approximate_age: params[:pet][:approximate_age],
+            sex: params[:pet][:sex],
+            shelter_id: params[:id],
+            adoption_status: "Adopatable"
+            })
+    pet.save
+
+    redirect_to "/shelters/#{params[:id]}/pets"
+  end
+
 end
