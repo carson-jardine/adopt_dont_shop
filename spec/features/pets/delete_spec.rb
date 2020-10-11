da538edb6b99038ec32bff61a61ef85a8233105a
Carson Jardine <carsonjardine@gmail.com>
2020-10-11 13:37:30 -0600

    Create delete spec and set up test

---
+++ spec/features/pets/delete_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  before :each do
+    @shelter_1 = Shelter.create(
+        name:     "The Dumb Friends League",
+        address:  "2080 S Quebec Street",
+        city:     "Denver",
+        state:    "CO",
+        zip:      "80231")
+    @pet_1 = Pet.create(
+        image: "http://cdn.akc.org/content/hero/cute_puppies_hero.jpg",
+        name:  "Louis",
+        approximate_age: 2,
+        sex: "Male",
+        shelter_id: "#{@shelter_1.id}")
+    @pet_2 = Pet.create(
+        image: "http://cdn.akc.org/content/article-body-image/wirehaired_pointing_griffon_cute_puppies.jpg",
+        name:  "Charlie",
+        approximate_age: 5,
+        sex: "Male",
+        shelter_id: "#{@shelter_1.id}")
+  end
+
+
+end
