69b044e24130ebbf6a9c7d65638e4103ed70d9a1
Carson Jardine <carsonjardine@gmail.com>
2020-10-11 14:49:31 -0600

    Update tests to include edit link on index

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
 describe 'As a visitor' do
-  before :each do
-    @shelter_1 = Shelter.create(
-        name:     "The Dumb Friends League",
-        address:  "2080 S Quebec Street",
-        city:     "Denver",
-        state:    "CO",
-        zip:      "80231")
-    @pet_1 = Pet.create(
-        image: "http://cdn.akc.org/content/hero/cute_puppies_hero.jpg",
-        name:  "Louis",
-        approximate_age: 2,
-        sex: "Male",
-        shelter_id: "#{@shelter_1.id}")
-    @pet_2 = Pet.create(
-        image: "http://cdn.akc.org/content/article-body-image/wirehaired_pointing_griffon_cute_puppies.jpg",
-        name:  "Charlie",
-        approximate_age: 5,
-        sex: "Male",
-        shelter_id: "#{@shelter_1.id}")
-  end
-
   describe 'When I visit /pets' do
+    before :each do
+      @shelter_1 = Shelter.create(
+          name:     "The Dumb Friends League",
+          address:  "2080 S Quebec Street",
+          city:     "Denver",
+          state:    "CO",
+          zip:      "80231")
+      @pet_1 = Pet.create(
+          image: "http://cdn.akc.org/content/hero/cute_puppies_hero.jpg",
+          name:  "Louis",
+          approximate_age: 2,
+          sex: "Male",
+          shelter_id: "#{@shelter_1.id}")
+      @pet_2 = Pet.create(
+          image: "http://cdn.akc.org/content/article-body-image/wirehaired_pointing_griffon_cute_puppies.jpg",
+          name:  "Charlie",
+          approximate_age: 5,
+          sex: "Male",
+          shelter_id: "#{@shelter_1.id}")
+    end
+
     it "The visitor sees the details about each pet in system" do
 
       visit "/pets"
@@ -32,6 +32,16 @@ describe 'As a visitor' do
       expect(page).to have_content("#{@pet_1.approximate_age}")
       expect(page).to have_content("#{@pet_1.sex}")
       expect(page).to have_content("#{@pet_1.shelter.name}")
+    end
+
+    it "The visitor sees an edit link next to each pet" do
+      visit "/pets"
+
+      expect(page).to have_link("Edit: #{@pet_1.name}")
+      expect(page).to have_link("Edit: #{@pet_2.name}")
+
+      click_link("Edit: #{@pet_1.name}")
+      expect(current_path).to eq("/pets/#{@pet_1.id}/edit")
     end
   end
 end
