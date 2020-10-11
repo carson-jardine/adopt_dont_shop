82981c517fedfbd7d45536aca0d5038e29260cf5
Carson Jardine <carsonjardine@gmail.com>
2020-10-11 13:30:35 -0600

    Add update method to controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,4 +10,19 @@ class PetsController < ApplicationController
   def edit
     @pet = Pet.find(params[:id])
   end
+
+  def update
+    pet = Pet.find(params[:id])
+    pet.update({
+      name: params[:pet][:name],
+      image: params[:pet][:image],
+      description: params[:pet][:description],
+      approximate_age: params[:pet][:approximate_age],
+      sex: params[:pet][:sex],
+      })
+
+    pet.save
+
+    redirect_to "/pets/#{pet.id}"
+  end
 end
