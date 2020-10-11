b1f20736d88688053ad1df3c3c9677429e5abd3c
Carson Jardine <carsonjardine@gmail.com>
2020-10-11 13:25:26 -0600

    Update code to include pets update route

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
   get '/pets/:id/edit', to: 'pets#edit'
+  patch '/pets/:id', to: 'pets#update'
 
   #shelter_pets
   get '/shelters/:id/pets', to: 'shelter_pets#index'
