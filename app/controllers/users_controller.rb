8a43d3c604443f7b4a09bedcb1a840319c6f971f
Carson Jardine <carsonjardine@gmail.com>
2020-10-13 17:58:40 -0600

    Update controller and view to include new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,21 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+  end
 
+  def new
+  end
+
+  def create
+    user = User.new({
+                name: params[:user][:name],
+                address: params[:user][:address],
+                city: params[:user][:city],
+                state: params[:user][:state],
+                zip: params[:user][:zip]
+                })
+    user.save
+
+    redirect_to "/users/#{user.id}"
   end
 end
