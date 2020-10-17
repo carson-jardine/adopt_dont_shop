4be6e854f43465413aa9e1c5ddeb3f5e7bc87b1c
Carson Jardine <carsonjardine@gmail.com>
2020-10-17 11:18:52 -0600

    Create user spec and add tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe 'relationships' do
+    it { should have_many :reviews}
+  end
+
+  describe 'instance methods' do
+    before :each do
+
+    @shelter_1 = Shelter.create!({
+        name:    "The Feline Fix",
+        address:  "6075 Parkway Drive",
+        city:     "Denver",
+        state:    "CO",
+        zip:      "80022"})
+    @shelter_2 = Shelter.create!({
+          name:    "The Dumb Friends League",
+          address:  "2080 S Quebec Street",
+          city:     "Denver",
+          state:    "CO",
+          zip:      "80231"})
+    @user_1 = User.create!(
+        name:     "Mike Dao",
+        address:  "123 Taylor Swift Ave",
+        city:     "Denver",
+        state:    "CO",
+        zip:      "80213")
+    @review_1 = Review.create!({
+        title: "Best Place Ever",
+        rating: 5,
+        content: "The vets were nice af",
+        optional_image: "https://sayingimages.com/wp-content/uploads/You-Got-It-meme.jpg",
+        reviewer_name: "#{@user_1.name}",
+        user_id: "#{@user_1.id}",
+        shelter_id: "#{@shelter_2.id}"})
+    @review_2 = Review.create!({
+        title: "Worst Place Ever",
+        rating: 1,
+        content: "The vets were stupid af",
+        optional_image: "https://sayingimages.com/wp-content/uploads/You-Got-It-meme.jpg",
+        reviewer_name: "#{@user_1.name}",
+        user_id: "#{@user_1.id}",
+        shelter_id: "#{@shelter_1.id}"})
+    @review_3 = Review.create!({
+        title: "Seen better",
+        rating: 2,
+        content: "The vets were nice but stupid",
+        optional_image: "https://sayingimages.com/wp-content/uploads/You-Got-It-meme.jpg",
+        reviewer_name: "#{@user_1.name}",
+        user_id: "#{@user_1.id}",
+        shelter_id: "#{@shelter_1.id}"})
+    end
+
+    it "#average_user_rating" do
+      expect(@user_1.average_user_rating.round(2)).to eq(2.67)
+    end
+  end
+end
