298905dd69577efbb21f8b5fa64d61bda5dd0414
Carson Jardine <carsonjardine@gmail.com>
2020-10-20 19:06:40 -0600

    Update code with stats

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,5 +1,15 @@
 class Shelter < ApplicationRecord
   validates_presence_of :name
   has_many :pets
-  has_many :reviews 
+  has_many :reviews
+
+  def average_shelter_rating
+    reviews.average(:rating)
+  end
+
+  def total_applications
+    pets.count do |pet|
+      pet.applications
+    end
+  end
 end
